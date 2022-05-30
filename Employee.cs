c3742347200d1b3328aec4eb217f2f43f208c2f7
Brad Haertzen <brhaertzen@gmail.com>
2022-05-29 21:24:52 -0400

    Employee class fleshed out to include constructor and required methods. Program class includes creating 5 Employees and running their functions.

---
+++ Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Week_4_Exercise
+{
+	class Employee
+	{
+		//Made these public so we could retrieve their title for our second working method
+		public string firstName;
+		public string lastName;
+		public string fullName;
+		public string startDate;
+		public string title;
+		public string salary;
+		
+		public Employee(string _firstName, string _lastName, string _startDate, string _title, string _salary)
+		{
+			firstName = _firstName;
+			lastName = _lastName;
+			fullName = firstName + " " + lastName;
+			startDate = _startDate;
+			title = _title;
+			salary = _salary;
+
+			//Lets us know it was properly created
+			Console.WriteLine(fullName + " was created.");
+		}
+
+		//So we would know who is telling their info
+		public void DeclareName()
+		{
+			Console.WriteLine("My name is " + fullName + ".");
+		}
+
+		public void Working()
+		{
+			Console.WriteLine("I am working.");
+		}
+
+		public void Working(string project)
+		{
+			Console.WriteLine("I am working on " + project);
+		}
+
+		public void ShowTitle()
+		{
+			Console.WriteLine("My title is " + title + ".");
+		}
+
+		public void ShowSalary()
+		{
+			Console.WriteLine("I have been working here since " + startDate + " and make " + salary + ".");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Week_4_Exercise
 {
@@ -6,7 +7,33 @@ namespace Week_4_Exercise
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<Employee> employees = new List<Employee>();
+
+			Employee phillip = new Employee("Phillip", "Smith", "January 1st", "Water Boy", "$35,000");
+			employees.Add(phillip);
+
+			Employee ashley = new Employee("Ashley", "Johnson", "March 2nd", "CEO", "$150,000");
+			employees.Add(ashley);
+
+			Employee daniel = new Employee("Daniel", "Davis", "April 3rd", "Digital Artist", "$75,000");
+			employees.Add(daniel);
+
+			Employee sarah = new Employee("Sarah", "Taylor", "May 4th", "Accountant", "$90,000");
+			employees.Add(sarah);
+
+			Employee nicholas = new Employee("Nicholas", "Taylor", "June 5th", "Architect", "$110,000");
+			employees.Add(nicholas);
+
+			Console.WriteLine();
+			foreach (Employee employee in employees)
+			{
+				employee.DeclareName();
+				employee.Working();
+				employee.Working("being the best " + employee.title + " in the world.");
+				employee.ShowTitle();
+				employee.ShowSalary();
+				Console.WriteLine();
+			}
 		}
 	}
 }
